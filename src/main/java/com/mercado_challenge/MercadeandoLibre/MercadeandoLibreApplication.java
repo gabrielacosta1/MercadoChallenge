2c58fcbf7d2f2949c4174311cd29ba524ed8d180
Gabriel Acosta <acostagabriel126@gmail.com>
2025-09-12 00:16:17 -0300

    Initial commit

---
+++ src/main/java/com/mercado_challenge/MercadeandoLibre/MercadeandoLibreApplication.java
@@ -0,0 +1,13 @@
+package com.mercado_challenge.MercadeandoLibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadeandoLibreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadeandoLibreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercado_challenge/MercadeandoLibre/MercadeandoLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado_challenge.MercadeandoLibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadeandoLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
