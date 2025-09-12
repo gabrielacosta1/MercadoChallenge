a9ff7ce18afbe2ec0b5255b524713169be203f4f
Gabriel Acosta <acostagabriel126@gmail.com>
2025-09-12 15:21:34 -0300

    modelos de entidades

--- src/main/java/com/mercado_challenge/MercadoAdventure/MercadoAdventureApplication.java
+++ src/main/java/com/mercado_challenge/MercadoAdventure/MercadoAdventureApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MercadeandoLibreApplication {
+public class MercadoAdventureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MercadeandoLibreApplication.class, args);
+		SpringApplication.run(MercadoAdventureApplication.class, args);
 	}
 
 }
